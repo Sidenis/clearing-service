7123586cb00f89dc5ccb099bcc8108ee8589a7cb
Tiemo Hunger <tiemo.hunger@sidenis.com>
2019-07-02 15:48:38 +0200

    rules applied input

--- server.js
+++ server.js
@@ -1,27 +1,82 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const PORT = process.env.PORT || 5000;
+const TIME_OUT = 1500;
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
+	enum LOB {
+		LIABILITY
+		PROPERTY
+	}
+
+	enum STATUS {
+		DONE
+		PROGRESS
+		MANUAL
+	}
+
+	enum PERIL {
+		FIRE
+		NAT_CAT
+		TERRORISM
+	}
+
+	type RuleApplied {
+		rule:String
+		status:STATUS
+	}
+	input RulesAppliedInput{
+		rule:String
+		status:STATUS
+	}
+	type Submission {
+		id: ID!
+		lob: LOB!
+		country: String!
+		insuredCompany: String!
+		address: String!
+		broker: String!
+		peril: PERIL!
+		rules:[RuleApplied]!
+	}
+	input SubmissionInput {
+		id: ID!
+		lob: LOB!
+		country: String!
+		insuredCompany: String!
+		address: String!
+		broker: String!
+		peril: PERIL!
+		rules:[RulesAppliedInput]!
+	}
 	type Mutation {
-		clearWithRules(rules: [String], submisson: String): [String]
+		clearWithRules(submission:SubmissionInput): Boolean
 	}
 
 	type Query {
-		currentClearings: [String]
+		currentClearing: Submission
 	}
 `;
+let currentClearing = null;
 
 const resolvers = {
 	Mutation: {
 		clearWithRules: (_, { rules, submission }) => {
-			return [];
+			if (currentClearing) {
+				return false;
+			} else {
+				currentClearing = {
+					rules,
+					submission
+				};
+				return true;
+			}
 		}
 	},
 	Query: {
-		currentClearings() {
+		currentClearing() {
 			return [];
 		}
 	}
